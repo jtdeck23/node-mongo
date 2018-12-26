9c5063868fee01c749ad3d61b093af2358c00503
jtdeck23 <jordandeck060@gmail.com>
2018-12-25 23:35:23 -0600

    setting app to use heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -27,8 +30,33 @@ app.get('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } 
+
+        res.send({todo});
+    }, (e) => {
+        res.status(404).send();
+    });
+    // find by id
+        //success
+            //if todo - send it back
+            // if no todo - send 404 - empty body
+        //error
+            //400 - send empty
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
